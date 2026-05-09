2c8e76646d6995457eafe17f4ec2c4951951528f
boniek.junior <boniek.junior.123@gmail.com>
2026-05-09 19:08:05 -0300

    fix: Adicionando a pasta app dentro das pasta de frontend.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from core.database import Base, engine
 from api.router import api_router
+from contextlib import asynccontextmanager
 
 # Criando a instância do FastAPI
 app = FastAPI(
@@ -13,7 +14,11 @@ app = FastAPI(
 app.include_router(api_router)
 
 # Evento de inicialização para criar as tabelas do banco de dados
-@app.on_event("startup")
-def on_startup() -> None:
-    """Cria as tabelas do banco de dados ao iniciar a aplicação."""
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Código a ser executado ao iniciar a aplicação
     Base.metadata.create_all(bind=engine)
+    yield
+    # Código a ser executado ao encerrar a aplicação
+
+app = FastAPI(lifespan=lifespan)
