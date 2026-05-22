ff608b3c82854b05c893c25edabdad5242756511
Lucas Fernandes Bezerra de Melo <lfbm@cin.ufpe.br>
2026-05-21 23:27:48 -0300

    configuração do banco

--- core/database.py
+++ core/database.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from core.config import settings
 
 # Configuração do banco de dados
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
+database_url = settings.DATABASE_URL
+connect_args = {}
+if database_url.startswith("sqlite"):
+    # sqlite requires this arg for use with the same connection in multiple threads
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(database_url, connect_args=connect_args)
 
 # Criando a sessão do SQLAlchemy
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
